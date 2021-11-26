7c93e301cb699a98bb66582793c40ba8a8c892bc
Alexander Sklyar <AlexanderSGSklyar@mail.ru>
2021-11-26 19:03:11 +0300

    Firstfix

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -2,16 +2,16 @@ package ru.netology.sqr;
 
 public class SQRService {
     int calc(int firstNum, int secondNum) {
-       int count = 0;
+        int count = 0;
         for (int i = 10; i <= 99; i++) {
             if (i * i >= firstNum && i * i <= secondNum) {
                 count++;
             }
-            }
-
-System.out.println(count);
-        return count;
         }
 
-
+        System.out.println(count);
+        return count;
     }
+
+
+}
--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -5,11 +5,31 @@ import static org.junit.jupiter.api.Assertions.*;
 class SQRServiceTest {
 
     @org.junit.jupiter.api.Test
-    void calcSqrt() {
+    void shouldCountNum() {
         SQRService sqrService = new SQRService();
         int expected = 3;
         int actual = sqrService.calc(200, 300);
 
         assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldCountNumNoResult() {
+        SQRService sqrService = new SQRService();
+        int expected = 0;
+        int actual = sqrService.calc(10, 99);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldCountNumOneResult() {
+        SQRService sqrService = new SQRService();
+        int expected = 1;
+        int actual = sqrService.calc(10, 100);
+
+        assertEquals(expected, actual);
+
     }
 }
